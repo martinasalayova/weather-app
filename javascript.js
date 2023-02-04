042fe3ba9dccfbebf0e5390389e03e961ca3f678
martinasalayova <vachkovamartina@gmail.com>
2023-02-04 17:44:49 +0100

    Cleaned code and fixed bug.

--- javascript.js
+++ javascript.js
@@ -11,7 +11,7 @@ function submitACity(event) {
   let apiKey = "ab8a5768a15f9a84dce115b22d102c9d";
   let units = "metric";
   let city = `${searchInput.value}`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   console.log(`${apiUrl}`);
   axios.get(apiUrl).then(weather);
 }
@@ -97,7 +97,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "b9ba0314a93083136d968577c718e31d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -131,7 +131,7 @@ function showPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = "ab8a5768a15f9a84dce115b22d102c9d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
 function formatGeoDate(timestamp) {
@@ -175,6 +175,7 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 let submitCurrentLocation = document.querySelector("#current-location-button");
@@ -230,7 +231,7 @@ function defaultTemp() {
   let apiKey = "ab8a5768a15f9a84dce115b22d102c9d";
   let units = "metric";
   let city = `Hradec Kralove`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(weather);
 }
 defaultTemp();
